d9e5e643980dbfdda947bd6751da779464224ae3
Alaric Hunziker <alaric.hunz@utexas.edu>
2022-11-09 17:49:56 -0600

    close to finishing the weather part

--- app/src/main/java/com/sosa/final_project/ui/WeatherFragment.kt
+++ app/src/main/java/com/sosa/final_project/ui/WeatherFragment.kt
@@ -1,13 +1,16 @@
 package com.sosa.final_project.ui
 
+import android.os.AsyncTask
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.fragment.app.Fragment
 import com.sosa.final_project.databinding.FragmentWeatherBinding
+import org.json.JSONObject
+import java.net.URL
+import java.text.SimpleDateFormat
+import java.util.*
 
 /**
  * A simple [Fragment] subclass.
@@ -24,8 +27,77 @@ class WeatherFragment : Fragment() {
     ): View? {
         //Get binding
         _binding = FragmentWeatherBinding.inflate(inflater, container, false)
-
         //Inflate the layout for this fragment
         return binding.root
+    }
+
+    inner class weatherTask() : AsyncTask<String, Void, String>() {
+        override fun onPreExecute() {
+            super.onPreExecute()
+            /* Showing the ProgressBar, Making the main design GONE */
+            binding.loader.visibility = View.VISIBLE
+            binding.mainContainer.visibility = View.GONE
+            binding.errorText.visibility = View.GONE
+        }
+
+        override fun doInBackground(vararg params: String?): String? {
+            var response:String?
+            try{
+                response =
+                    URL("api.openweathermap.org/data/2.5/forecast?lat=30.267153&lon=-97.743057&appid=9ad69cc6290a831d72d65d24bac82167")
+                    .readText(Charsets.UTF_8)
+            } catch (e: Exception){
+                response = null
+            }
+            return response
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            try {
+                /* Extracting JSON returns from the API */
+                val jsonObj = JSONObject(result)
+                val main = jsonObj.getJSONObject("main")
+                val sys = jsonObj.getJSONObject("sys")
+                val wind = jsonObj.getJSONObject("wind")
+                val weather = jsonObj.getJSONArray("weather").getJSONObject(0)
+
+                val updatedAt:Long = jsonObj.getLong("dt")
+                val updatedAtText = "Updated at: "+ SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(Date(updatedAt*1000))
+                val temp = main.getString("temp")+"°C"
+                val tempMin = "Min Temp: " + main.getString("temp_min")+"°C"
+                val tempMax = "Max Temp: " + main.getString("temp_max")+"°C"
+                val pressure = main.getString("pressure")
+                val humidity = main.getString("humidity")
+
+                val sunrise:Long = sys.getLong("sunrise")
+                val sunset:Long = sys.getLong("sunset")
+                val windSpeed = wind.getString("speed")
+                val weatherDescription = weather.getString("description")
+
+                val address = jsonObj.getString("name")+", "+sys.getString("country")
+
+                /* Populating extracted data into our views */
+                binding.address.text = address
+                binding.updatedAt.text = updatedAtText
+                binding.status.text = weatherDescription.capitalize()
+                binding.temp.text = temp
+                binding.tempMin.text = tempMin
+                binding.tempMax.text = tempMax
+                binding.sunrise.text = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunrise*1000))
+                binding.sunset.text = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunset*1000))
+                binding.wind.text = windSpeed
+                binding.pressure.text = pressure
+                binding.humidity.text = humidity
+
+                /* Views populated, Hiding the loader, Showing the main design */
+                binding.loader.visibility = View.GONE
+                binding.mainContainer.visibility = View.VISIBLE
+
+            } catch (e: Exception) {
+                binding.loader.visibility = View.GONE
+                binding.errorText.visibility = View.VISIBLE
+            }
+        }
     }
 }
