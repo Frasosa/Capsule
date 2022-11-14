8b11d2f9309867b868bc62636a1a50dc896b9f7e
Alaric Hunziker <alaric.hunz@utexas.edu>
2022-11-13 18:13:49 -0600

    This version doesn't work but ideally it has all the info we need

--- app/src/main/java/com/sosa/final_project/ui/WeatherFragment.kt
+++ app/src/main/java/com/sosa/final_project/ui/WeatherFragment.kt
@@ -21,9 +21,6 @@ import java.util.*
  */
 class WeatherFragment : Fragment() {
 
-    val CITY: String = "austin, tx, us"
-    val API: String = "06c921750b9a82d8f5d1294e1586276f"
-
     private var _binding: FragmentWeatherBinding? = null
     private val binding get() = _binding!!
 
@@ -56,7 +53,6 @@ class WeatherFragment : Fragment() {
             var response:String?
             try{
                 response =
-                    // https://api.openweathermap.org/data/2.5/weather?q=$CITY&units=metric&appid=$API
                     URL("http://api.openweathermap.org/data/2.5/forecast?lat=30.267153&lon=-97.743057&appid=06c921750b9a82d8f5d1294e1586276f&units=imperial")
                     .readText(Charsets.UTF_8)
             } catch (e: Exception){
@@ -127,7 +123,7 @@ class WeatherFragment : Fragment() {
                 val address = JSONObject(result).getJSONObject("city")
                 val city = address.getString("name")
                 val country = address.getString("country")
-                val fullAddress = city + ", " + country
+                val fullAddress = "$city, $country"
 
                 // Populating extracted data into our views
                 binding.address.text = fullAddress
@@ -154,45 +150,6 @@ class WeatherFragment : Fragment() {
 
                 binding.day6.text = updatedAt6Text
                 binding.day6Temp.text = temp6
-
-//                binding.sunrise.text = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunrise*1000))
-//                binding.sunset.text = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunset*1000))
-//                binding.wind.text = windSpeed
-//                binding.pressure.text = pressure
-//                binding.humidity.text = humidity
-
-//                val main = jsonObj.getJSONObject("main")
-//                val sys = jsonObj.getJSONObject("sys")
-//                val wind = jsonObj.getJSONObject("wind")
-//                val weather = jsonObj.getJSONArray("weather").getJSONObject(0)
-//
-//                val updatedAt:Long = jsonObj.getLong("dt")
-//                val updatedAtText = "Updated at: "+ SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(Date(updatedAt*1000))
-//                val temp = main.getString("temp")+"°C"
-//                val tempMin = "Min Temp: " + main.getString("temp_min")+"°C"
-//                val tempMax = "Max Temp: " + main.getString("temp_max")+"°C"
-//                val pressure = main.getString("pressure")
-//                val humidity = main.getString("humidity")
-//
-//                val sunrise:Long = sys.getLong("sunrise")
-//                val sunset:Long = sys.getLong("sunset")
-//                val windSpeed = wind.getString("speed")
-//                val weatherDescription = weather.getString("description")
-//
-//                val address = jsonObj.getString("name")+", "+sys.getString("country")
-//
-//                /* Populating extracted data into our views */
-//                binding.address.text = address
-//                binding.updatedAt.text = updatedAtText
-//                binding.status.text = weatherDescription.capitalize()
-//                binding.temp.text = temp
-//                binding.tempMin.text = tempMin
-//                binding.tempMax.text = tempMax
-//                binding.sunrise.text = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunrise*1000))
-//                binding.sunset.text = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunset*1000))
-//                binding.wind.text = windSpeed
-//                binding.pressure.text = pressure
-//                binding.humidity.text = humidity
 
                 /* Views populated, Hiding the loader, Showing the main design */
                 binding.loader.visibility = View.GONE
